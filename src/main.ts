85446ccf079459c1db0cb4a878c81f8104395da2
Ben Daly <mvben33@gmail.com>
2023-10-14 18:22:09 -0700

    First fork commit, hello!!

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,11 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "My game";
 
+
+
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+app.append(document.createElement("h2").textContent = "hello hello! :3");
