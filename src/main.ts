ae2ef5ba68317cc82b0343a350abe8610c162fab
Ben Daly <mvben33@gmail.com>
2023-10-16 13:12:29 -0700

    Step 4 done but events are still VERY broken

--- src/main.ts
+++ src/main.ts
@@ -31,6 +31,14 @@ const clearButton = document.createElement("button");
 clearButton.innerHTML = "clear";
 app.append(clearButton);
 
+const undoButton = document.createElement("button");
+undoButton.innerHTML = "undo";
+app.append(undoButton);
+
+const redoButton = document.createElement("button");
+redoButton.innerHTML = "redo";
+app.append(redoButton);
+
 interface MousePoints{
     x: number
     y: number
@@ -38,7 +46,7 @@ interface MousePoints{
 
 // Make an array of array of mouse points.
 const mousePointsArraySquared: MousePoints[][] = [];
-let strokeNum = 0;
+let undoRedoStack: MousePoints[][] = [];
 
 // Cursor is pretty much just an arbitrary data structure to store mouse state data.
 const cursor = { active: false, x: 0, y: 0 };
@@ -50,6 +58,7 @@ canvas.addEventListener("mousedown", (e) => {
     cursor.y = e.offsetY;
 
     mousePointsArraySquared.push([]);
+    canvas.dispatchEvent(drawingChangedEvent);
 });
 
 canvas.addEventListener("mousemove", (e) => {
@@ -58,7 +67,7 @@ if (cursor.active) {
     cursor.y = e.offsetY;
     const mousePoint: MousePoints = { x: cursor.x, y: cursor.y };
     // store the mouse point
-    mousePointsArraySquared[strokeNum].push(mousePoint);
+    mousePointsArraySquared[mousePointsArraySquared.length - 1].push(mousePoint);
 
     // begin drawing process
     dispatchEvent(drawingChangedEvent);
@@ -71,16 +80,17 @@ function drawToCanvas() {
     ctx.fillRect(zero, zero, canvas.width, canvas.height);
 
     const one = 1;
+    console.log(mousePointsArraySquared);
 
     // foreach set of points in the mouse array
     for (let i = zero; i < mousePointsArraySquared.length; i++) {
         for (let j = zero; j < mousePointsArraySquared[i].length; j++) {
             if (j == zero) {
-                ctx.moveTo(mousePointsArraySquared[i][j].x, mousePointsArraySquared[i][j].y); }
-            else if (j == mousePointsArraySquared[i].length - one) {
+                ctx.moveTo(mousePointsArraySquared[i][j].x, mousePointsArraySquared[i][j].y);
+            } else if (j == mousePointsArraySquared[i].length - one) {
                 ctx.lineTo(mousePointsArraySquared[i][j].x, mousePointsArraySquared[i][j].y);
-                ctx.stroke(); }
-            else {
+                ctx.stroke();
+            } else {
                 ctx.lineTo(mousePointsArraySquared[i][j].x, mousePointsArraySquared[i][j].y);
             }
         }
@@ -92,17 +102,37 @@ canvas.addEventListener("mouseup", () => {
     // resetting state
     cursor.active = false;
     // Update the stroke num in the list
-    strokeNum += 1;
 
-
+    console.log(mousePointsArraySquared);
+    canvas.dispatchEvent(drawingChangedEvent);
 });
 
 
 clearButton.addEventListener("click", () => {
-ctx.clearRect(zero, zero, canvas.width, canvas.height);
-ctx.fillRect(zero, zero, canvasSize, canvasSize);
+    ctx.clearRect(zero, zero, canvas.width, canvas.height);
+    ctx.fillRect(zero, zero, canvasSize, canvasSize);
+    undoRedoStack = [];
+    mousePointsArraySquared.length = 0;
+});
+
+undoButton.addEventListener("click", () => {
+    if (mousePointsArraySquared.length > zero) {
+        undoRedoStack.push(mousePointsArraySquared.pop()!); 
+        canvas.dispatchEvent(drawingChangedEvent);
+        console.log(mousePointsArraySquared);
+    }
+
+});
+
+redoButton.addEventListener("click", () => {
+    if (undoRedoStack.length > zero) {
+        mousePointsArraySquared.push(undoRedoStack.pop()!);
+        canvas.dispatchEvent(drawingChangedEvent);
+        console.log(mousePointsArraySquared);
+    }
 });
 
 canvas.addEventListener("drawing-changed", () => {
     drawToCanvas();
 });
+
