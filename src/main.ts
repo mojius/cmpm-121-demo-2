8355b3fc3ce881491c0db0a0d15cf99def1a13fa
Ben Daly <mvben33@gmail.com>
2023-10-16 12:32:11 -0700

    Step 3 complete (with broken event logic, ignoring inexplicable and stupid eslint mandates for now)

--- src/main.ts
+++ src/main.ts
@@ -16,56 +16,93 @@ app.append(header);
 const divvy = document.createElement("div");
 app.append(divvy);
 
-
-
 const canvas = document.createElement("canvas");
 const ctx = canvas.getContext("2d")!;
 ctx.canvas.width = 256;
 ctx.canvas.height = 256;
 canvas.classList.add("canvas");
 divvy.append(canvas);
+ctx.fillStyle = "white";
+ctx.fillRect(zero, zero, canvasSize, canvasSize);
+
+const drawingChangedEvent: Event = new Event("drawing-changed");
 
 const clearButton = document.createElement("button");
 clearButton.innerHTML = "clear";
 app.append(clearButton);
 
-ctx.fillStyle = "white";
-ctx.fillRect(zero, zero, canvasSize, canvasSize);
+interface MousePoints{
+    x: number
+    y: number
+}
 
+// Make an array of array of mouse points.
+const mousePointsArraySquared: MousePoints[][] = [];
+let strokeNum = 0;
 
 // Cursor is pretty much just an arbitrary data structure to store mouse state data.
 const cursor = { active: false, x: 0, y: 0 };
 
 canvas.addEventListener("mousedown", (e) => {
-// Setting state and old position
-cursor.active = true;
-cursor.x = e.offsetX;
-cursor.y = e.offsetY;
+    // Setting state and old position
+    cursor.active = true;
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+
+    mousePointsArraySquared.push([]);
 });
 
 canvas.addEventListener("mousemove", (e) => {
 if (cursor.active) {
-    ctx.beginPath();
-    // moveTo() starts the origin of a new subpath
-    ctx.moveTo(cursor.x, cursor.y);
-    // lineTo() takes the old position as a starting point and draws a line to the new position.
-    ctx.lineTo(e.offsetX, e.offsetY);
-    // stroke() fills that li'l line path we just made with virtual ink.
-    ctx.stroke();
     cursor.x = e.offsetX;
     cursor.y = e.offsetY;
+    const mousePoint: MousePoints = { x: cursor.x, y: cursor.y };
+    // store the mouse point
+    mousePointsArraySquared[strokeNum].push(mousePoint);
+
+    // begin drawing process
+    dispatchEvent(drawingChangedEvent);
+
 }
 });
 
+function drawToCanvas() {
+    ctx.clearRect(zero, zero, canvas.width, canvas.height);
+    ctx.fillRect(zero, zero, canvas.width, canvas.height);
+
+    const one = 1;
+
+    // foreach set of points in the mouse array
+    for (let i = zero; i < mousePointsArraySquared.length; i++) {
+        for (let j = zero; j < mousePointsArraySquared[i].length; j++) {
+            if (j == zero) {
+                ctx.moveTo(mousePointsArraySquared[i][j].x, mousePointsArraySquared[i][j].y); }
+            else if (j == mousePointsArraySquared[i].length - one) {
+                ctx.lineTo(mousePointsArraySquared[i][j].x, mousePointsArraySquared[i][j].y);
+                ctx.stroke(); }
+            else {
+                ctx.lineTo(mousePointsArraySquared[i][j].x, mousePointsArraySquared[i][j].y);
+            }
+        }
+    }
+    // iterate through and draw them all
+}
+
 canvas.addEventListener("mouseup", () => {
-// resetting state
-cursor.active = false;
+    // resetting state
+    cursor.active = false;
+    // Update the stroke num in the list
+    strokeNum += 1;
+
+
 });
 
 
-
-// Clears the rendering context.
 clearButton.addEventListener("click", () => {
-    ctx.clearRect(zero, zero, canvas.width, canvas.height);
-    ctx.fillRect(zero, zero, canvasSize, canvasSize);
+ctx.clearRect(zero, zero, canvas.width, canvas.height);
+ctx.fillRect(zero, zero, canvasSize, canvasSize);
+});
+
+canvas.addEventListener("drawing-changed", () => {
+    drawToCanvas();
 });
